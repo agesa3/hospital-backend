445dfaf3859ccf270d4a6594654e81e95e6d09cb
agesa3 <collinsagesa2000@gmail.com>
2022-09-27 10:13:36 +0300

    Project setup and initilalization

---
+++ src/main/java/com/agesadev/healthIt/HealthItApplication.java
@@ -0,0 +1,13 @@
+package com.agesadev.healthIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agesadev/healthIt/HealthItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agesadev.healthIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
